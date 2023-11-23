9314e5852d0ccd0bfd3fee10fc22f36863a26593
Emerson Patryck da Silva <emerson.silva@docstage.com.br>
2023-11-23 15:20:54 -0300

    initial commit

---
+++ app/Http/Controllers/ApplicationnController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreApplicationnRequest;
+use App\Http\Requests\UpdateApplicationnRequest;
+use App\Models\Applicationn;
+
+class ApplicationnController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreApplicationnRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Applicationn $applicationn)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Applicationn $applicationn)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateApplicationnRequest $request, Applicationn $applicationn)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Applicationn $applicationn)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/VacineController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVacineRequest;
+use App\Http\Requests\UpdateVacineRequest;
+use App\Models\Vacine;
+
+class VacineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVacineRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Vacine $vacine)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Vacine $vacine)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVacineRequest $request, Vacine $vacine)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Vacine $vacine)
+    {
+        //
+    }
+}
